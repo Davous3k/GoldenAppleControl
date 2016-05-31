66d87c0acb2cb362d88e78642eb200d7027ae73b
Terrobility <huntergplays@gmail.com>
2016-05-31 21:23:45 +0100

    Removed reload command

--- src/main/java/net/terrocidepvp/goldenapplecontrol/commands/CommandManager.java
+++ src/main/java/net/terrocidepvp/goldenapplecontrol/commands/CommandManager.java
@@ -1,7 +1,6 @@
 package net.terrocidepvp.goldenapplecontrol.commands;
 
 import net.terrocidepvp.goldenapplecontrol.GoldenAppleControl;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,19 +10,6 @@ public class CommandManager implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-
-        if (args.length >= 1) {
-            String firstLetterFromArg = args[0].substring(0, 1);
-            if (firstLetterFromArg.equalsIgnoreCase("r")) {
-                if (sender.hasPermission("goldenapplecontrol.reload")) {
-                    sender.sendMessage(ChatColor.GREEN + "Attempting to reload config...");
-                    GoldenAppleControl.getInstance().reloadConfig();
-                    return true;
-                } else {
-                    sender.sendMessage(GoldenAppleControl.getInstance().getNoPerm());
-                }
-            }
-        }
 
         if (args.length == 0) {
             if (sender instanceof Player) {
